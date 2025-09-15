f63fd3d2c88ac04c41e3efa0bf9326b193a1aa56
santos2607 <ssacchetti@mail.austral.edu.ar>
2025-09-15 12:17:55 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
-# Replace the "ANSWER HERE" with your answer
+
 
 def remove_elements(list_to_remove_elements):
-    if list_to_remove_elements !="":
+    if list_to_remove_elements != '':
         if len(list_to_remove_elements) >= 6:
             del list_remove_to_remove_elements[5]
         if len(list_to_remove_elements) >= 5:
@@ -15,27 +15,27 @@ def remove_elements(list_to_remove_elements):
 
 
 def add_elements(list_to_add_elements):
-    list_to_add_elements.insert(0,"Pink")
-    list_to_add_elements.append("Yellow")
+    list_to_add_elements.insert(0,'Pink')
+    list_to_add_elements.append('Yellow')
     return list_to_add_elements
 
 
 
 def is_empty(list_to_check):
-   if len(list_to_check) == 0
+   return len(list_to_check) == 0
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) <3 or len(list_to_compare2) < 3:
-        return false
+        return False
     return list_to_compare1[2] == list_to_compare2[2]
 
 
 def list_of_lists(list_of_lists_to_modify):
-    if len(list_of_lists_to_modify) >0 and len(list_of_lists_to_modify[0]) >= 2:
+    if len(list_of_lists_to_modify) > 0 and len(list_of_lists_to_modify[0]) >= 2:
         list_of_lists_to_modify[0] = list_of_lists_to_modify[0][:2]
 
     if len(list_of_lists_to_modify) > 1:
-        if len(list_of_lists_to_modify[1]) >=4:
+        if len(list_of_lists_to_modify[1]) >= 4:
             list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
         elif len(list_of_lists_to_modify[1]) > 1:
             list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:]
